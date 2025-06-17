0900dd3d62c605a94d7f41d8d1c494956c7400ff
gosapp <gosapps86contact@gmail.com>
2025-06-17 19:26:50 -0300

    .

--- myApp.js
+++ myApp.js
@@ -2,8 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-
+app.use(helmet.hidePoweredBy());  // Hide the X-Powered-By header
+app.use(helmet.frameguard({action: 'deny'})); // Prevent clickjacking by denying framing
+app.use(helmet.xssFilter()); // Enable XSS filtering
+app.use(helmet.noSniff()); // Prevent browsers from MIME-sniffing and override the content-type header
+app.use(helmet.ieNoOpen()); // Prevent Internet Explorer from executing downloads in a frame
+ninetyDaysInSeconds = 90*24*60*60; // 90 days in seconds
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+})); // Enable HTTP Strict Transport Security (HSTS) with a max age of 90 days
 
 
 
